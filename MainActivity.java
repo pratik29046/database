0d025e33520dfc213fafe58333aa3eb00e90625f
pratik29046 <79098295+pratik29046@users.noreply.github.com>
2022-03-15 19:22:29 +0530

    Add files via upload
    datas

--- MainActivity.java
+++ MainActivity.java
@@ -1,137 +1,60 @@
-package com.example.sql1;
+package com.abhayjeet.listview;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseHelper mdb;
-    EditText name,add,phone,id;
-    Button save,show,update,delete;
-    TextView temp;
-
+    String data[]={"apple","banana","mango"};
+    ArrayList<String> lists= new ArrayList<>();
+    ArrayAdapter<String> ad;
+    ListView list;
+    TextView texts;
+    EditText edit,edit2;
+    Button btn1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mdb=new DatabaseHelper(this);
-        id=findViewById(R.id.id);
-        name=findViewById(R.id.name);
-        add=findViewById(R.id.add);
-        phone=findViewById(R.id.phone);
-        save=findViewById(R.id.save);
-        show=findViewById(R.id.show);
-        update=findViewById(R.id.update);
-        delete=findViewById(R.id.delete);
-        temp=findViewById(R.id.temp);
-        adds();
-//        shows();
-//        updates();
-//        deletes();
+        list=findViewById(R.id.listview);
+        texts=findViewById(R.id.text);
+        edit=findViewById(R.id.edits1);
 
-        show.setOnClickListener(new View.OnClickListener() {
+//        edit2=findViewById(R.id.edits1);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent i=new Intent(getApplicationContext(),MainActivity2.class);
-                startActivity(i);
+            public void onClick(View v) {
+                String e=edit.getText().toString();
+//                String s=edit2.getText().toString();
+                lists.add(e);
+                edit.setText("");
+
             }
         });
 
 
-    }
-
-    public void adds(){
-        save.setOnClickListener(new View.OnClickListener() {
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-               boolean b= mdb.insert(id.getText().toString(),name.getText().toString(),add.getText().toString(),phone.getText().toString());
-               if(b== true){
-                   Toast.makeText(getApplicationContext(), "save data", Toast.LENGTH_SHORT).show();
-                   id.setText("");
-                   name.setText("");
-                   add.setText("");
-                   phone.setText("");
-               }
-               else{
-                   Toast.makeText(getApplicationContext(), "data is not save", Toast.LENGTH_SHORT).show();
-               }
+            public void onClick(View v) {
+                show();
             }
         });
+
     }
-
-//    public void shows(){
-//        show.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Cursor res= mdb.data_get();
-//                if(res.getCount()==-1){
-////                    message("Error","data is not found");
-//                    return;
-//                }
-//                StringBuffer sb=new StringBuffer();
-//                while(res.moveToNext()){
-//                    sb.append("ID "+res.getString(0)+"\n");
-//                    sb.append("name "+res.getString(1)+"\n");
-//                    sb.append("address "+res.getString(2)+"\n");
-//                    sb.append("phone "+res.getString(3)+"\n");
-//                }
-////                message("data",sb.toString());
-//                temp.setText(sb.toString());
-//            }
-//        });
-//    }
-
-    public void message(String titles,String message){
-        AlertDialog.Builder b=new AlertDialog.Builder(this);
-        b.setCancelable(true);
-        b.setTitle(titles);
-        b.setMessage(message);
-        b.show();
+    public void show(){
+        ad=new ArrayAdapter<String>(MainActivity.this,R.layout.activity_main2,R.id.text2,lists);
+        list.setAdapter(ad);
     }
-
-
-//    public void updates(){
-//        update.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                boolean up=mdb.update(id.getText().toString(),name.getText().toString(),add.getText().toString(),phone.getText().toString());
-//                if(up == true){
-//                    Toast.makeText(getApplicationContext(), "data is updated", Toast.LENGTH_SHORT).show();
-//                    id.setText("");
-//                    name.setText("");
-//                    add.setText("");
-//                    phone.setText("");
-//                }
-//                else{
-//                    Toast.makeText(getApplicationContext(), "data is not updated", Toast.LENGTH_SHORT).show();
-//                }
-//
-//            }
-//        });
-//    }
-
-//    public void deletes(){
-//        delete.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//               Integer row= mdb.delete(id.getText().toString());
-//               if(row>=0){
-//                   Toast.makeText(getApplicationContext(), "data is deleted", Toast.LENGTH_SHORT).show();
-//               }else{
-//                   Toast.makeText(getApplicationContext(), "data is not deleted", Toast.LENGTH_SHORT).show();
-//               }
-//            }
-//        });
-//    }
 }
